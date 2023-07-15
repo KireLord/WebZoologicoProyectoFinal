cad9cbfa776b99815be02ab7fa48b3cb312dd014
Erick Rosero <roseroerick15@gmail.com>
2023-07-15 12:18:06 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebZoologico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
